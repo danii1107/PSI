27893273c4819761ba770a88d8bdac89e5472c1c
enriquegf506 <enriquegf506@gmail.com>
2024-04-03 13:03:35 +0200

    Test_models only 1 error

--- mychess/models/admin.py
+++ mychess/models/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Player, ChessGame, ChessMove
+
+admin.site.register(Player)
+admin.site.register(ChessGame)
+admin.site.register(ChessMove)
 
 # Register your models here.
--- mychess/models/models.py
+++ mychess/models/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Player(AbstractUser):
-    rating = models.IntegerField(default=0)
+    rating = models.IntegerField(default = -1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -13,41 +13,44 @@ class Player(AbstractUser):
         verbose_name_plural = 'Players'
 
     def __str__(self):
-        return self.username
+        return f"{self.username} ({self.rating})"
 
-""" class ChessGame(models.Model):
+class ChessGame(models.Model):
     STATUS = (
     ('pending', 'pending'),
     ('active', 'active'),
     ('finished', 'finished'),
 )
+    DEFAULT_BOARD_STATE = chess.Board().fen()
+
 
     status = models.CharField(max_length=10, choices=STATUS, default='pending')
-    board_state = models.CharField(max_length=100)
+    board_state = models.CharField(max_length=100, default = DEFAULT_BOARD_STATE)
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True, blank=True)
     timeControl = models.CharField(max_length=50)
-    blackPlayer = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='black_games')
-    whitePlayer = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='white_games')
+    blackPlayer = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='black_games', null = True)
+    whitePlayer = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='white_games', null = True)
     winner = models.ForeignKey(Player, on_delete=models.CASCADE, null=True, blank=True, related_name='won_games')
 
     def __str__(self):
-        return f"Partida de Ajedrez entre {self.white_player.username} (Blancas) y {self.black_player.username} (Negras)"
+        white_Player = self.whitePlayer if self.whitePlayer else 'unknown'
+        black_Player = self.blackPlayer if self.blackPlayer else 'unknown'
+        return  f'GameID=({self.id}) {white_Player} vs {black_Player}'
 
 class ChessMove(models.Model):
     game = models.ForeignKey(ChessGame, on_delete=models.CASCADE)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     move_from = models.CharField(max_length=2)
     move_to = models.CharField(max_length=2)
-    promotion = models.CharField(max_length=1, blank=True, null=True)
+    promotion = models.CharField(max_length=1, blank=True, null=True, default = '')
 
     def __str__(self):
-        player_color = "Blancas" if self.player == self.game.white_player else "Negras"
         move_description = f"{self.move_from} -> {self.move_to}"
         if self.promotion:
             promotion_piece = self.promotion.lower()
             move_description += f" (Promoción a {promotion_piece})"
-        return f"Jugador: {self.player.username} ({player_color}), Movimiento: {move_description}"
+        return f"{self.player}: {move_description}"
 
     def save(self, *args, **kwargs):
         chess_game = self.game
@@ -58,12 +61,18 @@ class ChessMove(models.Model):
         board = chess.Board(chess_game.board_state)
         from_square = chess.parse_square(self.move_from)
         to_square = chess.parse_square(self.move_to)
-        move = chess.Move(from_square, to_square)
-        if move not in board.legal_moves:
+
+        if self.promotion:
+            move = chess.Move(from_square, to_square, promotion=self.promotion)
+        else:
+            move = chess.Move(from_square, to_square)
+
+
+        if not board.is_legal(move):
             raise ValueError("Movimiento no válido.")
+
         board.push(move)
         chess_game.board_state = board.fen()
         chess_game.save()
 
         super().save(*args, **kwargs)
- """
