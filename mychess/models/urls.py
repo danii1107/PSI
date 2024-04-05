1086122c582e425eb96d490d4541aafb518f6d50
enriquegf506 <enriquegf506@gmail.com>
2024-04-05 11:57:52 +0200

    tests new

--- mychess/models/urls.py
+++ mychess/models/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from models.mychess import myclassView
 from models.api import MyTokenCreateView, ChessGameViewSet
 
 urlpatterns = [
-    path(r'myclassView/', myclassView.as_view()),
     path( 'mytokenlogin/', MyTokenCreateView.as_view()),
     path('games/', ChessGameViewSet.as_view({'get': 'list', 'post': 'create'}), name='games'),
     path('games/<int:pk>/', ChessGameViewSet.as_view({'get': 'retrieve'}), name='games-detail'),
