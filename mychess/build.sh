2c6b54ffeaf9cf2ef5bbc7efa07afe1728cb8c54
danii1107 <danielbarsan72003@gmail.com>
2024-04-08 16:40:05 +0200

    allowed hosts problems, no err if superuser does not exist when try to delete it

--- mychess/build.sh
+++ mychess/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-export DJANGO_SUPERUSER_EMAIL=alumnodb@alumnodb.com
-export DJANGO_SUPERUSER_USERNAME=alumnodb
-export DJANGO_SUPERUSER_PASSWORD=alumnodb
+export DJANGO_SUPERUSER_EMAIL='alumnodb@alumnodb.com'
+export DJANGO_SUPERUSER_USERNAME='alumnodb'
+export DJANGO_SUPERUSER_PASSWORD='alumnodb'
 python3.11 manage.py shell -c "from models.models import Player; Player.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').delete()"
-python3.11 manage.py createsuperuser --no-input
+if python3.11 manage.py createsuperuser --no-input; then
+    echo "Superusuario creado con éxito."
+else
+    echo "No se pudo crear el superusuario. Es posible que ya exista o haya un problema con las variables de entorno."
+fi
 daphne -b 0.0.0.0 mychess.asgi:application
