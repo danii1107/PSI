4c0e6462f43e368bba74df406176a83cc08efc67
danii1107 <danielbarsan72003@gmail.com>
2024-04-08 16:09:12 +0200

    render superuser 2nd try, new build

--- mychess/build.sh
+++ mychess/build.sh
@@ -2,5 +2,6 @@
 export DJANGO_SUPERUSER_EMAIL='alumnodb@alumnodb.com'
 export DJANGO_SUPERUSER_USERNAME='alumnodb'
 export DJANGO_SUPERUSER_PASSWORD='alumnodb'
-python3.11 manage.py createsuperuser --noinput
+python3.11 manage.py shell -c "from models.models import Player; Player.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').delete()"
+python3.11 manage.py createsuperuser --no-input
 daphne -b 0.0.0.0 mychess.asgi:application
