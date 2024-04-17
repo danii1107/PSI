e81a14be6cdf85442b8e189d45c5a9bad37c5f60
danii1107 <danielbarsan72003@gmail.com>
2024-04-17 17:44:26 +0200

    component & view separated

--- mychess-vue/src/components/LogIn.vue
+++ mychess-vue/src/components/LogIn.vue
@@ -1,21 +1,62 @@
 <template>
-		<div class="background">
-			<!-- Your login component content here -->
-			<LogInView />
-		</div>
+	<div class="container">
+		<form v-if="!showRegisterForm" @submit.prevent="login">
+			<input type="email" v-model="loginEmail" placeholder="Correo electrónico" required>
+			<input type="password" v-model="loginPassword" placeholder="Contraseña" required>
+			<button type="submit">Iniciar Sesión</button>
+		</form>
+		<p class="register" v-if="!showRegisterForm">¿No tienes cuenta? <span class="toggle" @click="toggleForm">Regístrate aquí</span></p>
+	</div>
 </template>
 
 <script>
-import LogInView from '../views/LogInView.vue';
-
-export default {
-	name: 'LogIn',
-	components: {
-		LogInView
-	},
-	// Your component's logic here
-}
+	export default {
+		data() {
+			return {
+				loginEmail: '',
+				loginPassword: ''
+			};
+		},
+		methods: {
+			handleLogin() {
+				console.log('Login');
+			},
+			toggleForm() {
+				this.$router.push('/sign-up');
+			}
+		}
+	}
 </script>
 
 <style scoped>
+	.container {
+		margin-right: 20px;
+		width: 50%;
+		padding: 20px;
+		text-align: center;
+	}
+	input, button {
+		width: 80%;
+		padding: 12px 20px;
+		margin: 8px 0;
+		display: inline-block;
+		border: 1px solid #ccc;
+		box-sizing: border-box;
+	}
+	button {
+		background-color: #6d260a;
+		color: white;
+		border: none;
+		cursor: pointer;
+	}
+	button:hover {
+		opacity: 0.8;
+	}
+	.toggle {
+		color: whitesmoke;
+		cursor: pointer;
+	}
+	.register {
+		color: white;
+	}
 </style>
--- mychess-vue/src/views/LogInView.vue
+++ mychess-vue/src/views/LogInView.vue
@@ -1,81 +1,30 @@
 <template>
 	<div class="background-image">
-		<div class="container">
-		<h2>Bienvenido/a</h2>
-		<form v-if="!showRegisterForm" @submit.prevent="login">
-			<input type="email" v-model="loginEmail" placeholder="Correo electrónico" required>
-			<input type="password" v-model="loginPassword" placeholder="Contraseña" required>
-			<button type="submit">Iniciar Sesión</button>
-		</form>
-		<p class="register" v-if="!showRegisterForm">¿No tienes cuenta? <span class="toggle" @click="toggleForm">Regístrate aquí</span></p>
-		</div>
-		</div>
-  </template>
+	  <log-in @login="handleLogin"></log-in>
+	</div>
+</template>
   
-  <script>
-  export default {
-	data() {
-	  return {
-		showRegisterForm: false,
-		loginEmail: '',
-		loginPassword: '',
-		registerFullName: '',
-		registerEmail: '',
-		registerPassword: '',
-	  };
-	},
-	methods: {
-	  toggleForm() {
-		this.$router.push("/sign-up");
-	  },
-	  login() {
-		alert('Login attempted');
-	  },
-	},
-  };
-  </script>
-  
-  <style scoped>
-  .container {
-	background-color: transparent;
-	margin: auto;
-	width: 50%;
-	padding: 20px;
-	text-align: center;
-  }
-  input, button {
-	width: 80%;
-	padding: 12px 20px;
-	margin: 8px 0;
-	display: inline-block;
-	border: 1px solid #ccc;
-	box-sizing: border-box;
-  }
-  button {
-	background-color: #4CAF50;
-	color: white;
-	border: none;
-	cursor: pointer;
-  }
-  button:hover {
-	opacity: 0.8;
-  }
-  .toggle {
-	color: whitesmoke;
-	cursor: pointer;
-  }
+<script>
+	import LogIn from '../components/LogIn.vue';
 
-.background-image {
-    width: 100vw;
-    height: 100vh;
-    background-image: url('../assets/logn-bg.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-}
-.register {
-	color: white;
-}
+	export default {
+		components: {
+			LogIn
+		},
+	}
+</script>
   
-  </style>
+<style scoped>
+	.background-image {
+		display: flex;
+		align-items: center; /* Alinea verticalmente al centro */
+		justify-content: flex-end; /* Alinea horizontalmente a la derecha */
+		width: 100vw;
+		height: 100vh;
+		background-image: url('../assets/logn-bg.jpg');
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center;
+	} 
+</style>
   
