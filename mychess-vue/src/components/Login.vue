9993a5c080bbe7b3787da2aeb364dede0d66ac2a
danii1107 <danielbarsan72003@gmail.com>
2024-04-20 21:50:00 +0200

    login style

--- mychess-vue/src/components/Login.vue
+++ mychess-vue/src/components/Login.vue
@@ -1,9 +1,20 @@
 <template>
-	<div class="container">
+	<div class="wrapper">
+		<h2>Welcome</h2>
 		<form @submit.prevent="handleLogin">
-			<input type="email" v-model="persona.email" placeholder="Correo electrónico" required>
-			<input type="password" v-model="persona.password" placeholder="Contraseña" required>
-			<button type="submit">Iniciar Sesión</button>
+			<div class="input-field">
+				<input type="email" id="email" placeholder="E-mail" required>
+				<i class='bx bxs-user'></i>
+			</div>
+			<div class="input-field">
+				<input type="password" id="password" placeholder="Password" required>
+				<i class='bx bxs-lock-alt'></i>
+			</div>
+			<a href="#" class="forgot">
+				<p>Forgot password?</p>
+			</a>
+			<button type="submit" class="login">Login</button>
+			<p class="sign-up">Don't have an account? <a href="#" class="sign-up">sign up</a></p>
 		</form>
 	</div>
 </template>
@@ -33,27 +44,104 @@
 </script>
 
 <style scoped>
-	.container {
-		margin-right: 20px;
-		width: 50%;
-		padding: 20px;
+	.wrapper {
+		position: absolute;
+		top: 50%;
+		left: 75%;
+		transform: translate(-50%, -50%);
+		width: 350px;
+		height: 350px;
 		text-align: center;
+		border: 1px solid rgb(241, 241, 241);
+		border-radius: 12px;
+		padding: 10px 20px;
+		background: transparent;
+		backdrop-filter: blur(6px);
+		box-shadow: 5px 5px 10px 0 rgba(255, 255, 255, 0.5);
 	}
-	input, button {
-		width: 80%;
-		padding: 12px 20px;
-		margin: 8px 0;
-		display: inline-block;
-		border: 1px solid #ccc;
-		box-sizing: border-box;
+
+	.wrapper h2 { 
+		font-size: 30px;
+		color: #fffefe;
 	}
-	button {
-		background-color: #6d260a;
-		color: white;
+
+	.input-field {
+		position: relative;
+	}
+
+	.input-field input[type="email"],
+	.input-field input[type="password"] {
+		border-radius: 10px;
+		background: transparent;
+		margin: 15px;
+		border: 2px solid rgb(255, 255, 255);
+		width: 280px;
+		height: 2px;
+		padding: 20px 20px 20px 20px;
+		backdrop-filter: blur(15px);
+		color: rgb(255, 255, 255);
+	}
+
+	.input-field i {
+		position: absolute;
+		top: 50%;
+		right: 10px;
+		transform: translateY(-50%);
+		color: rgb(252, 252, 252);
+	}
+
+	input::placeholder {
+		color: rgb(255, 255, 255);
+	}
+
+	.input-field input[type="email"]:focus::placeholder,
+	.input-field input[type="password"]:focus::placeholder {
+		transform: translateY(-100%);
+		transition: transform 0.2s ease-in-out;
+		font-size: 14px;
+	}
+
+	.input-field input[type="email"]:focus::placeholder,
+	.input-field input[type="password"]:focus::placeholder {
+		transform: translateY(0%);
+		transition: transform 0.2s ease-in-out;
+		font-size: 16px;
+	}
+
+	a.forgot {
+		color: rgb(255, 255, 255, 0.712);
+		text-decoration: none;
+		position: relative;
+	}
+
+	a.forgot:hover {
+		text-decoration: underline;
+		color: #fffffffd;
+	}
+
+	p.sign-up {
+		color: rgb(255, 255, 255, 0.712);
+	}
+
+	a.sign-up {
+		color: rgb(255, 255, 255, 0.712);
+		text-decoration: none;
+	}
+
+	a.sign-up:hover {
+		text-decoration: underline;
+		color: #fffffffd;
+	}
+
+	.wrapper .login {
+		background: #fff;
 		border: none;
+		outline: none;
 		cursor: pointer;
+		font-weight: 600;
+		border-radius: 45px;
+		width: 200px;
+		height: 30px;
 	}
-	button:hover {
-		opacity: 0.8;
-	}
+
 </style>
