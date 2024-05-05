243a84aee77b348690be2ec3073b2b79fe896ee4
danii1107 <danielbarsan72003@gmail.com>
2024-05-05 20:15:24 +0200

    not creating a new game if u have one started

--- mychess-vue/src/views/CreateGameView.vue
+++ mychess-vue/src/views/CreateGameView.vue
@@ -16,26 +16,49 @@ import { useTokenStore } from '../stores/token';
 			const apiURL = import.meta.env.VITE_DJANGOURL;
 			const tokenStore = useTokenStore();
 			let gameData = null;
+			let games = null;
 
 			const consumeAPInew = async (callback) => {
-				console.log(tokenStore.token);
 				try {
-					const response = await fetch(`${apiURL}/api/v1/games/`, {
-						method: 'POST',
+					const responseget = await fetch(`${apiURL}/api/v1/games/`, {
+						method: 'GET',
 						headers: {
 							'Authorization': 'token ' + tokenStore.token,
 							'Accept': 'application/json',
 							'Content-Type': 'application/json' 
 						},
-						body: JSON.stringify({})
 					});
-					if (response.ok) {
-						gameData = await response.json();
-						tokenStore.setGameData(gameData);
-						console.log(gameData);
-						callback(true); 
+					if (responseget.ok){
+						games = await responseget.json();
+						for (const game of games) {
+							console.log("Checking game:", game);
+							if (game.status !== 'Finished' && (game.whitePlayer === tokenStore.user_id || game.blackPlayer === tokenStore.user_id)) {
+								console.log("Active game found:", game);
+								tokenStore.setGameData(game);
+								callback(true);
+								return;
+							}
+						}
+						const response = await fetch(`${apiURL}/api/v1/games/`, {
+							method: 'POST',
+							headers: {
+								'Authorization': 'token ' + tokenStore.token,
+								'Accept': 'application/json',
+								'Content-Type': 'application/json' 
+							},
+							body: JSON.stringify({})
+						});
+						if (response.ok) {
+							gameData = await response.json();
+							tokenStore.setGameData(gameData);
+							console.log(gameData);
+							callback(true); 
+						} else {
+							console.error('Failed to join or create game:', response.status);
+							callback(false, response.status);
+						}
 					} else {
-						console.error('Failed to join or create game:', response.status);
+						console.error('Failed to list games:', response.status);
 						callback(false, response.status);
 					}
 				} catch (error) {
